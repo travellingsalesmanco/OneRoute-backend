c495fbd242eb7df17f9aafb13278a94f48a16773
Yu Jia Tay <yujiatay@gmail.com>
2017-09-20 13:14:28 +0800

    Minor changes

--- server.js
+++ server.js
@@ -11,17 +11,17 @@ var express = require('express'),
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://127.0.0.1/onemapdb', { useMongoClient: true });
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-var onemap = require('./app/onemap/omRoutes'); //importing route
-onemap(app); //register the route
-
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow'Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var onemap = require('./app/onemap/omRoutes'); //importing route
+onemap(app); //register the route
 
 app.listen(port);
 
