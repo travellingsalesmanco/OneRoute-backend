7fbb299f5205b2dc231aba69bebec505c42f9ed4
Yu Jia Tay <yujiatay@gmail.com>
2017-09-22 15:21:40 +0800

    Convert ajax calls

--- app/lib/api_calls.js
+++ app/lib/api_calls.js
@@ -1,4 +1,7 @@
 'use strict';
+var turf = require('@turf/turf');
+var polyline = require('@mapbox/polyline');
+var request = require("request");
 
 exports.request_route = function (req, res) {
     var start_point = req.query.start;
@@ -21,75 +24,76 @@ function routeReq (start, end, mode) {
     var endcoord = end[1] + "," + end[0];
     // var startcoord = start;
     // var endcoord = end;
-    $.ajax({
+    var route_options = {
         method: "GET",
         url: "http://onemap.duckdns.org/route",
         data: {
             start: startcoord,
             end: endcoord,
-            routeType: mode,
-        },
+            routeType: mode
+        }
+    };
 
-        success: function (data, status) {
-            var result = JSON.parse(data);
-            var parsed_result = {};
+    request(route_options, function(err, response, body) {
+        if (err) throw new Error(err);
+        console.log(body);
+        var result = JSON.parse(body);
+        var parsed_result = {};
 
-            function featuregeom(featurejson) {
-                return {
-                    "type": "LineString",
-                    "coordinates": polyline.decode(featurejson["route_geometry"]),
-                    "properties": {
-                        "route_instructions": featurejson["route_instructions"],
-                        "route_name": featurejson["route_name"],
-                        "route_summary": featurejson["route_summary"]
-                    }
+        function featuregeom(featurejson) {
+            return {
+                "type": "LineString",
+                "coordinates": polyline.decode(featurejson["route_geometry"]),
+                "properties": {
+                    "route_instructions": featurejson["route_instructions"],
+                    "route_name": featurejson["route_name"],
+                    "route_summary": featurejson["route_summary"]
                 }
             }
-
-            parsed_result["main"] = turf.feature(featuregeom(result));
-            parsed_result["alternative"] = [];
-            for (var i = 0; i < result["alternativeroute"].length; i++) {
-                parsed_result["alternative"][i] = turf.feature(featuregeom(result["alternativeroute"][i]))
-            }
-            console.log(status);
-            console.log(parsed_result);
-            return parsed_result;
         }
+
+        parsed_result["main"] = turf.feature(featuregeom(result));
+        parsed_result["alternative"] = [];
+        for (var i = 0; i < result["alternativeroute"].length; i++) {
+            parsed_result["alternative"][i] = turf.feature(featuregeom(result["alternativeroute"][i]))
+        }
+        console.log(parsed_result);
+        return parsed_result;
     });
 }
 
 function searchReq (query) {
-    $.ajax({
+    var search_options = {
         method: "GET",
         url: "http://onemap.duckdns.org/search",
         data: {
             searchVal: query,
             getAddrDetails: "Y",
             returnGeom: "N"
-        },
+        }
+    };
 
-        success: function (data, status) {
-            var result = JSON.parse(data);
-            var array_of_features = [];
+    request(search_options, function(err, response, body) {
+        if (err) throw new Error(err);
+        console.log(body);
+        var result = JSON.parse(data);
+        var array_of_features = [];
 
-            function featurepoint(pointjson) {
-                return {
-                    "type": "Point",
-                    "coordinates": turf.Point(pointjson["LONGITUDE"], pointjson["LATITUDE"]),
-                    "properties": {
-                        "name": pointjson["SEARCHVAL"],
-                        "address": pointjson["ADDRESS"]
-                    }
+        function featurepoint(pointjson) {
+            return {
+                "type": "Point",
+                "coordinates": turf.Point(pointjson["LONGITUDE"], pointjson["LATITUDE"]),
+                "properties": {
+                    "name": pointjson["SEARCHVAL"],
+                    "address": pointjson["ADDRESS"]
                 }
             }
-
-            for (var i = 0; i < result["results"].length; i++) {
-                array_of_features[i] = turf.feature(featurepoint(result["results"][i]))
-            }
-            console.log(status);
-            console.log(array_of_features);
-            return array_of_features;
-
         }
+
+        for (var i = 0; i < result["results"].length; i++) {
+            array_of_features[i] = turf.feature(featurepoint(result["results"][i]))
+        }
+        console.log(array_of_features);
+        return array_of_features;
     });
 }
