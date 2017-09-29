9fbd52e50c49b5b61428d068cb9982c1641650c7
Yu Jia Tay <yujiatay@gmail.com>
2017-09-29 18:13:31 +0800

    Revert api calls to function calls

--- app/lib/api_calls.js
+++ app/lib/api_calls.js
@@ -3,10 +3,10 @@ var turf = require('@turf/turf');
 var polyline = require('@mapbox/polyline');
 var request = require("request");
 
-exports.request_route = function (req, res) {
-    var start_point = req.query.start;
-    var end_point = req.query.end;
-    var mode = req.query.mode;
+function routeReq (start, end, mode) {
+    var start_point = start;
+    var end_point = end;
+    var mode = mode;
 
     var route_options = {
         method: "GET",
@@ -17,12 +17,11 @@ exports.request_route = function (req, res) {
             routeType: mode
         }
     };
-
+    var parsed_result = {};
     request(route_options, function(err, response, body) {
         if (err) throw new Error(err);
         console.log(body);
         var result = JSON.parse(body);
-        var parsed_result = {};
 
         function featuregeom(featurejson) {
             return {
@@ -44,28 +43,28 @@ exports.request_route = function (req, res) {
             }
         }
         console.log(parsed_result);
-        res.send(parsed_result);
     });
-};
 
-exports.request_search = function (req, res) {
-    var search_value = req.query.searchVal;
+    return parsed_result;
+}
+
+function searchReq (searchVal) {
 
     var search_options = {
         method: "GET",
         url: "http://onemap.duckdns.org/search",
         qs: {
-            searchVal: search_value,
+            searchVal: searchVal,
             getAddrDetails: "Y",
             returnGeom: "N"
         }
     };
-
+    var array_of_features = [];
     request(search_options, function(err, response, body) {
         if (err) throw new Error(err);
         console.log(body);
         var result = JSON.parse(data);
-        var array_of_features = [];
+
 
         function featurepoint(pointjson) {
             return {
@@ -82,6 +81,7 @@ exports.request_search = function (req, res) {
             array_of_features[i] = turf.feature(featurepoint(result["results"][i]))
         }
         console.log(array_of_features);
-        res.send(array_of_features);
     });
-};
+
+    return array_of_features;
+}
--- app/onemap/omRoutes.js
+++ app/onemap/omRoutes.js
@@ -3,7 +3,6 @@
 module.exports = function(app) {
     var onemap = require('../onemap/omController');
     var onemap_auth = require('../onemap_auth/authController');
-    var api = require('../lib/api_calls');
 
     // One Map Routes
     app.route('/onemap/authtoken')
@@ -16,9 +15,5 @@ module.exports = function(app) {
         .get(onemap.draw_path);
 
     // API Routes
-    app.route('/api/route')
-        .get(api.request_route);
 
-    app.route('/api/search')
-        .get(api.request_search);
 };
